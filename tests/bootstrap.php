e5dfa3cfb28f8a337e1d3a5fe782bb9cf5a0aca5
Renaud Douze <renaud.douze@gmail.com>
2024-09-09 08:43:01 +0200

    Upgrade to phpunit/phpunit 11.3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require dirname(__DIR__).'/vendor/autoload.php';
+
+set_exception_handler([new ErrorHandler(), 'handleException']);
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
@@ -13,3 +16,4 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0o000);
 }
+
