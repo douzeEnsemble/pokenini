5a3764e0934ac6205d07ca88066dbb65cbb797c2
Renaud Douze <renaud.douze@gmail.com>
2024-09-25 22:19:27 +0200

    Add update collections availabilities on post build

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -11,12 +11,15 @@ php bin/console app:update:labels
 php bin/console app:update:games_and_dex
 php bin/console app:update:pokemons
 php bin/console app:update:regional_dex_numbers
-echo "Pause 61s" && sleep 60s && echo "Resume"
+echo "Pause 1 60s" && sleep 60s && echo "Resume 1 "
 php bin/console app:update:games_availabilities
 php bin/console app:update:games_shinies_availabilities
+php bin/console app:update:collections_availabilities
+echo "Pause 2 60s" && sleep 60s && echo "Resume 2"
 php bin/console app:calculate:game_bundles_availabilities
 php bin/console app:calculate:game_bundles_shinies_availabilities
 php bin/console app:calculate:dex_availabilities
 php bin/console app:calculate:pokemon_availabilities
 
 echo "End post_build scripts"
+App\Api\Message\UpdateCollectionsAvailabilities
