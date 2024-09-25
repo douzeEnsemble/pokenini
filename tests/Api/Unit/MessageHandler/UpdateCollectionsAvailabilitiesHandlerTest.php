ad4eded2dded3bbb73c65e08a2f31c7b10bbc59c
Renaud Douze <renaud.douze@gmail.com>
2024-09-25 22:20:09 +0200

    Add missing message handler

---
+++ tests/Api/Unit/MessageHandler/UpdateCollectionsAvailabilitiesHandlerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Api\Unit\MessageHandler;
+
+use App\Api\ActionEnder\ActionEnderTrait;
+use App\Api\Message\AbstractActionMessage;
+use App\Api\Message\UpdateCollectionsAvailabilities;
+use App\Api\MessageHandler\Traits\CalculateHandlerTrait;
+use App\Api\MessageHandler\UpdateCollectionsAvailabilitiesHandler;
+use App\Api\MessageHandler\UpdateHandlerInterface;
+use App\Api\Service\UpdaterService\CollectionsAvailabilitiesUpdaterService;
+use App\Api\Service\UpdaterService\UpdaterServiceInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversTrait;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+/**
+ * @internal
+ */
+#[CoversClass(UpdateCollectionsAvailabilitiesHandler::class)]
+#[UsesClass(CollectionsAvailabilitiesUpdaterService::class)]
+#[UsesClass(UpdateCollectionsAvailabilities::class)]
+#[CoversTrait(CalculateHandlerTrait::class)]
+#[CoversTrait(ActionEnderTrait::class)]
+class UpdateCollectionsAvailabilitiesHandlerTest extends AbstractTestUpdateHandler
+{
+    public function getServiceClass(): string
+    {
+        return CollectionsAvailabilitiesUpdaterService::class;
+    }
+
+    /**
+     * @param CollectionsAvailabilitiesUpdaterService $updaterService
+     */
+    public function getHandler(
+        UpdaterServiceInterface $updaterService,
+        EntityManagerInterface $entityManager,
+    ): UpdateHandlerInterface {
+        return new UpdateCollectionsAvailabilitiesHandler(
+            $updaterService,
+            $entityManager,
+        );
+    }
+
+    public function getMessage(): AbstractActionMessage
+    {
+        return new UpdateCollectionsAvailabilities('12');
+    }
+}
