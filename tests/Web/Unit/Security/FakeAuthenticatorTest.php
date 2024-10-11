9afc61dced0ee8d1d4421598ce22f346d69b5e53
Renaud Douze <renaud.douze@gmail.com>
2024-10-11 22:42:33 +0200

    Add a fake authenticator to avoid using Google Oauth in dev

---
+++ tests/Web/Unit/Security/FakeAuthenticatorTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Web\Unit\Security;
+
+use App\Web\Security\FakeAuthenticator;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * @internal
+ */
+#[CoversClass(FakeAuthenticator::class)]
+class FakeAuthenticatorTest extends TestCase
+{
+    public function testSupports(): void
+    {
+        $router = $this->createMock(RouterInterface::class);
+
+        $authenticator = new FakeAuthenticator(
+            $router,
+            'listAdmin',
+            'listTrainer',
+            'listCollector',
+        );
+
+        $this->assertTrue(
+            $authenticator->supports(
+                new Request([], [], ['_route' => 'app_web_connect_fakecheck'])
+            )
+        );
+        $this->assertFalse(
+            $authenticator->supports(
+                new Request([], [], ['_route' => 'app_web_connect_googlecheck'])
+            )
+        );
+    }
+
+    public function testStart(): void
+    {
+        $router = $this->createMock(RouterInterface::class);
+        $router
+            ->expects($this->once())
+            ->method('generate')
+            ->with('app_web_home_index')
+            ->willReturn('/home')
+        ;
+
+        $authenticator = new FakeAuthenticator(
+            $router,
+            'listAdmin',
+            'listTrainer',
+            'listCollector',
+        );
+
+        $request = new Request();
+
+        /** @var RedirectResponse $response */
+        $response = $authenticator->start($request);
+
+        $this->assertEquals('/home', $response->getTargetUrl());
+    }
+}
