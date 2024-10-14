96fefbec62e75372f81845d9cba32e40e7a88034
Renaud Douze <renaud.douze@gmail.com>
2024-10-14 21:43:56 +0200

    Fix is_premium migration

--- migrations/2024/10/Version20241006080300.php
+++ migrations/2024/10/Version20241006080300.php
@@ -16,7 +16,8 @@ final class Version20241006080300 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE dex ADD is_premium BOOLEAN NOT NULL');
+        $this->addSql('ALTER TABLE dex ADD is_premium BOOLEAN NOT NULL DEFAULT true');
+        $this->addSql('ALTER TABLE dex ALTER COLUMN is_premium DROP DEFAULT');
     }
 
     public function down(Schema $schema): void
