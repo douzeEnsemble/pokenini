1105efcab092e8564f399cb46fa1d284d5c0cf76
Renaud Douze <renaud.douze@gmail.com>
2025-01-04 13:45:35 +0100

    Add clickage area vote selecting

--- public/js/election.js
+++ public/js/election.js
@@ -26,11 +26,33 @@ function changeCardBorder(target) {
 }
 
 function updateElectionCounter() {
-  const checkboxes = document.querySelectorAll('input[name="winners_slugs[]"]');
+  const checkboxes = document.querySelectorAll('input[type="checkbox"][name="winners_slugs[]"');
   const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
   const count = checkedCheckboxes.length;
 
   document.getElementById('election-counter').textContent = count;
+}
+
+function watchCardClicking () {
+  document
+    .querySelectorAll('#election .election-card-image-container-regular')
+    .forEach(function (element) {
+      element.addEventListener('click', onCardClick);
+    });
+  document
+    .querySelectorAll('#election .list-group-item strong')
+    .forEach(function (element) {
+      element.addEventListener('click', onCardClick);
+    });
+}
+
+function onCardClick(event) {
+  event.preventDefault();
+
+  const target = event.target;
+  const card = target.closest('.card');
+
+  card.querySelector('input[type="checkbox"][name="winners_slugs[]"').click();
 }
 
 function watchSubmitAction () {
