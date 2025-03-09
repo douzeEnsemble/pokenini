1d6e6ef0812d7a3fb9b6608ef87e0554545fd12d
Renaud Douze <renaud.douze@gmail.com>
2025-03-09 18:06:49 +0100

    Refactor authentication's provider Controllers and Authenticators tests

--- src/Web/Controller/Connect/FacebookController.php
+++ src/Web/Controller/Connect/FacebookController.php
@@ -4,28 +4,18 @@ declare(strict_types=1);
 
 namespace App\Web\Controller\Connect;
 
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/connect/fb')]
-class FacebookController extends AbstractController implements ConnectControllerInterface
+class FacebookController extends AbstractConnectController
 {
-    #[Route('', methods: ['GET'])]
-    public function goto(ClientRegistry $clientRegistry): Response
+    protected function getProviderName(): string
     {
-        return $clientRegistry
-            ->getClient('facebook')
-            ->redirect(['openid'], [])
-        ;
+        return 'facebook';
     }
 
-    // @codeCoverageIgnoreStart
-    #[Route('/c', methods: ['GET'])]
-    public function check(): void
+    protected function getScope(): string
     {
-        // noting, all done by the authenticator
+        return 'openid';
     }
-    // @codeCoverageIgnoreEnd
 }
--- src/Web/Security/AmazonAuthenticator.php
+++ src/Web/Security/AmazonAuthenticator.php
@@ -4,51 +4,15 @@ declare(strict_types=1);
 
 namespace App\Web\Security;
 
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
-use Luchianenco\OAuth2\Client\Provider\AmazonResourceOwner;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
-
-class AmazonAuthenticator extends OAuth2Authenticator
+class AmazonAuthenticator extends AbstractAuthenticator
 {
-    use AuthenticatorTrait;
-
-    public function __construct(
-        private readonly ClientRegistry $clientRegistry,
-        private readonly RouterInterface $router,
-        private readonly string $listAdmin,
-        private readonly string $listTrainer,
-        private readonly string $listCollector,
-        private readonly bool $isInvitationRequired,
-    ) {}
-
-    public function supports(Request $request): ?bool
+    public function getProviderCode(): string
     {
-        return 'app_web_connect_amazon_check' === $request->attributes->get('_route');
+        return 'amazon';
     }
 
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function authenticate(Request $request): Passport
+    public function getProviderName(): string
     {
-        $client = $this->clientRegistry->getClient('amazon');
-        $accessToken = $this->fetchAccessToken($client);
-
-        return new SelfValidatingPassport(
-            new UserBadge($accessToken->getToken(), function () use ($accessToken, $client) {
-                /** @var AmazonResourceOwner $authUser */
-                $authUser = $client->fetchUserFromToken($accessToken);
-
-                /** @var string $userId */
-                $userId = $authUser->getId();
-
-                return $this->loadUserFromLists($userId, 'Amazon');
-            })
-        );
+        return 'Amazon';
     }
 }
